67b84066235d730a00d18aa990adcacb9290a302
Kaitlyn Concilio <kait@hey.com>
2014-10-22 15:47:09 -0400

    Initial commit
    Added readme, example, minified & source

---
+++ readme.md
@@ -0,0 +1,68 @@
+#SVGLite
+
+Use a combination of jQuery and CSS to do some basic style transformations with inline SVGs. Best use case is situations where you already have an SVG file (say, a map) and want to change the fill or stroke properties on the fly, or dynamically set the size of the element (including responsive).
+
+##Basic usage
+
+To get the party started, you need to include any settings (see below), then select your element with the jQuery selector and invoke the plugin thusly:  
+`$('#mySVG').svglite();`  
+Please note that this does almost nothing, but if you use the accompanying functions `pathclass` or `pathclass_pattern` you should call svglite first to set the properties.
+
+###Properties
+
+You have four basic properties to call here:  
+• `resize`: Whether you want the SVG to automatically resize based on the window size (true or false)  
+• `patterns`: If you're going to be using pattern fills for this element, you need to set the animation timing for the patterns here, in seconds. For more on this, see "Patterns".  
+• `default_opacity`: This setting is also used for patterns. The default opacity setting for your paths.  
+• `change_opacity'`: This is the path opacity you're changing to. (See "Patterns")  
+• `special`: If you have a specific size you want the SVG to be sized to, use this property with the name of the size you want. You'll declare the size in the settings, below.
+
+###Settings
+
+Including the plugin file will automatically create a global variable `svglitedefs` that we're going to use for a few global settings. 
+
+`svglitedefs.resize[str]`: This is the setting the 'special' property refers to. The example is if you have a CMS with a set width. This way we can declare a special setting "wordpress" to never go wider than 600 pixels with `svglitedefs.resize['wordpress'] = 600;`. Then, when you invoke the plugin, use `$('#svg').svglite({special:'wordpress'});`.  
+`svglitedefs.max`: Used with the responsive property to set a maximum width. If you want it to resize up to 1000px, set this to `1000`.
+
+##Class changes
+
+###Colors
+
+The other big thing that SVGLite provides is the ability to change the styling of your SVG paths. (Note: This may also work on polyfills, compound paths and groups, but it's only been tested on paths because that's all I needed it to do.)
+
+Most of this is done via CSS, but the provided secondary jQuery functions (pathclass and pathclass_pattern) provide an easy method to implement the necessary CSS changes (`$.addClass` and `$.removeClass` DO NOT WORK on SVGs. Trust me). The script automatically calls in the necessary transitions on all paths that are children of your main SVG element.
+
+Usage is pretty easy. If you're using just color fills (or changing the stroke), use `$.pathclass` thusly:  
+
+`$('#path_id').pathclass('class');`
+
+The default behavior is to replace all classes with the one provided. If you want to add a class while keeping the extant ones, use this:
+
+`$('#path_id').pathclass('class','add');`
+
+If you want to clear all classes, run the function with no parameters.
+
+`$('#path_id').pathclass();`
+
+If you want to remove a specific class, this:
+
+`$('#path_id').pathclass('class','remove');`
+
+###Patterns
+
+Patterns are the SVG implementation of background images. Because these must be defined [within the SVG](http://stackoverflow.com/a/3798797/645364) itself, the CSS fade we're using for colors doesn't work. Hence, if you're working with elements that are EVER going to use a pattern fill, you should use this second set of functions.
+
+Basically, we fake the transitions via the fill-opacity CSS property, which does exactly what you think it does. How the examples do it are pretty simple: everything starts at a mostly transparent color (0.1 black, which renders a light gray). You could also start with something super dark and fade in and out, but for our purposes we go light to dark.
+
+So our example would use
+
+`$('#svg).svglite({change_opacity:1,default_opacity:0.1});`
+
+And for our class changes we would use
+
+`$('#path_id').pathclass_pattern();`
+
+With the same rules that apply above in terms of adding and removing classes.
+
+##Limitations  
+I cannot stress enough the basic nature of this plugin. If you want to do anything other than change the fill, stroke or size of your SVG, this plugin **will not do jack for you**. I created it for my own uses for basic maps, and am loading up [ImageMagick-created PNGs](http://stackoverflow.com/questions/4809194/convert-svg-image-to-png-with-php) as fallbacks.
---
+++ svglite-0.1.js
@@ -0,0 +1,179 @@
+(function() {
+	$.fn.svglite = function (opts) {
+		if (this.prop('tagName') != 'svg') {
+			console.log("Not a valid SVGLite element");
+		}
+		else if (!this.attr('id')) {
+			this.attr('id') = 'svg'+Math.floor(date.getTime() * Math.random(0x10000));
+		}
+		else {
+			if (!this.attr('svglite')) {
+				if (!window.svglite) {
+					window.svglite = {};
+					window.svglite.obj = [];
+				}
+				$('<style>svg#'+$(this).attr('id')+' path { transition: fill 1.5s ease,fill-opacity 1.5s,stroke-width 0.5s; -webkit-backface-visiblity: hidden; -webkit-transition: fill 1.5s ease,fill-opacity 1.5s,stroke-width 0.5s;  }</style>').appendTo('html');
+				date = new Date;
+				var hash = Math.floor(date.getTime() * Math.random(0x10000));
+				this.attr('svglite',hash);
+				var settings = $.extend({
+					resize: false,
+					special: '',
+					patterns: false,
+					default_opacity: 1,
+					change_opacity: 1
+				},opts);
+				var obj = {};
+				obj.hash = hash;
+				obj.defaults = Array(parseInt(this.attr('height')),parseInt(this.attr('width')));
+				obj.resize = settings.resize;
+				obj.size = settings.special;
+				obj.patterns = settings.patterns;
+				obj.defopacity = settings.default_opacity;
+				obj.chopacity = settings.change_opacity;
+				window.svglite[hash] = obj;
+				window.svglite.obj.push(hash);
+				if (settings.resize) {
+					$(window).resize(function() {
+						svgresize(hash);
+					});
+				}
+				svgresize(hash,settings.special);
+				return this;
+			}
+
+			var hash = this.attr('svglite');
+			var settings = window.svglite[hash];
+			if (opts.resize != settings.resize) {
+				//Sir Not Needed In This Project
+			}
+			else if (opts.special != settings.special) {
+				//Sir Not Needed In This Project
+			}
+
+			function svgresize(id,style) {
+				if (window.svglitedefs[style]) {
+					svgw = svglitedefs[style]*.9;
+					svgh = svglitedefs[style]*.9*(window.svglite[id].defaults[0]/window.svglite[id].defaults[1]);
+				}
+				else if (svglitedefs.max) {
+					svgw = $(window).width()*.9;
+					svgw = (svgw > svglitedefs.max) ? svglitedefs.max : svgw;
+					svgh = svgw*(window.svglite[id].defaults[0]/window.svglite[id].defaults[1]);
+				}
+				else {
+					svgw = $(window).width()*.9;
+					svgh = svgw*(window.svglite[id].defaults[0]/window.svglite[id].defaults[1]);
+				}
+				if (window.svglitedefs.overflow == false) {
+					var th = $(window).height();
+					if (svgh > th) {
+						svgh = th * 0.9;
+						svgw = svgh*(window.svglite[id].defaults[1]/window.svglite[id].defaults[0]);
+					}
+				}
+				svg = $('svg[svglite='+hash+']');
+				svg.attr('viewbox','0,0,'+svgw+','+svgh+',true');
+				svg.attr('size','100%,100%');
+				$(svg).parent().attr({'width':svgw,'height':svgh});
+				$(svg).attr({'width':svgw,'height':svgh});
+			}
+		}
+	}
+
+	$.fn.pathclass_pattern = function(colorclass,type) {
+		var patterns = window.svglite[this.parents('svg').attr('svglite')].patterns;
+		var def_op = window.svglite[this.parents('svg').attr('svglite')].defopacity;
+		var ch_op = window.svglite[this.parents('svg').attr('svglite')].chopacity;
+		if (!colorclass) {
+			this.css({'fill-opacity':def_op});
+			var randhash = Math.floor(Math.random() * 0x10000000);
+			window.svglite[randhash] = this;
+			setTimeout(function() {
+				window.svglite_unset_patterns(randhash);
+			},patterns/3);
+			return this;
+		}
+		if (this.attr('class') == colorclass) {
+			return this;
+		}
+		else if (type == 'add') {
+			// If there's not one defined, skip to regular add class at bottom. Otherwise returns 'undefined' class
+			if (this.attr('class')) {
+				this.attr({'class':this.attr('class')+' '+colorclass}).this.css('fill-opacity',ch_op);	
+				return this;
+			}
+		}
+		// Remove a specific class
+		else if (type == 'remove') {
+			if (this.attr('class') == colorclass) {
+				this.removeAttr('class').css('fill-opacity',def_op);;
+				return this;
+			}
+			else if (this.attr('class')) {
+				this.css('fill-opacity',def_op).attr({'class':this.attr('class').replace(colorclass,'')}).css('fill-opacity',ch_op);	
+			}
+		}
+		// Default replaces all exsting classes with specified
+		if (this.attr('class')) {
+			this.css({'fill-opacity':def_op});
+			var randhash = Math.floor(Math.random() * 0x10000000);
+			window.svglite[randhash] = this;
+			setTimeout(function() {
+				window.svglite_set_patterns(randhash,colorclass);
+			},patterns/3);
+		}
+		else {
+			this.attr({'class':colorclass}).css('fill-opacity',ch_op);
+		}
+		return this;
+	}
+
+	$.fn.pathclass = function (colorclass,type) {
+		// If nothing, clear all styles
+		if (!colorclass) {
+			this.removeAttr('class');
+			return this;
+		}
+		// If adding a class
+		else if (type == 'add') {
+			if (this.attr('class') == colorclass) {
+				return this;
+			}
+			// If there's not one defined, skip to regular add class at bottom. Otherwise returns 'undefined' class
+			if (this.attr('class')) {
+				if ($.inArray(this.attr('class').split(' '),colorclass) == -1) {
+					this.attr({'class':this.attr('class')+' '+colorclass});	
+				}
+				return this;
+			}
+		}
+		// Remove a specific class
+		else if (type == 'remove') {
+			if (this.attr('class') == colorclass) {
+				this.removeAttr('class');
+				return this;
+			}
+			else if (this.attr('class')) {
+				var re = new RegExp('(\s?)'+colorclass+'(\s?)','g');
+				this.attr('class',this.attr('class').replace(re,''));
+				return this;
+			}
+		}
+		// Default replaces all exsting classes with specified
+		this.attr({'class':colorclass});
+		return this;
+	}
+	window.svglitedefs = {};
+	window.svglitedefs.resize = [];
+	window.svglitedefs.overflow = false;
+	
+	window.svglite_unset_patterns = function(obj) {
+		$(window.svglite[obj]).removeAttr('class');
+		window.svglite[obj] = false;
+	}
+	window.svglite_set_patterns = function(obj,color) {
+		$(window.svglite[obj]).removeAttr('class').attr('class',color).css({'fill-opacity':1});
+		window.svglite[obj] = false;
+	}
+})(jQuery);
